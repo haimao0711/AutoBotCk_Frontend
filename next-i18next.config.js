9dd2b1d4e9f1028da6a508fdf458bf609316871e
meobientnk34 <tranhaimaotnk34@gmail.com>
2025-08-17 00:01:37 +0700

    deploy fix 3

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,17 @@
+// used for SSR (getServerSideProps)
+// const path = require('path')
+// const localePath = path.resolve('./public/locales')
+
 module.exports = {
-  i18n: {
-    defaultLocale: 'en-US',
-    locales: ['en-US', 'de-DE', 'fr-FR', 'tr-TR'],
-    localeDetection: false,
-  },
-  nonExplicitSupportedLngs: true,
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
+	// https://www.i18next.com/overview/configuration-options#logging
+	/* debug: process.env.NODE_ENV === 'development', */
+	i18n: {
+		defaultLocale: 'en-US',
+		locales: ['en-US', 'de-DE', 'fr-FR', 'tr-TR'],
+		localeDetection: false,
+	},
+	nonExplicitSupportedLngs: true,
+	// localePath,
+	reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
+
